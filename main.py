b76baf51cac1c5397ac697194dd6f2b55e3bbba8
skynet <chu_liang_han@hotmail.com>
2025-02-19 14:26:19 +0800

    feat(streamlit_app): add the validator version
    add version function

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import streamlit as st
 import bittensor as bt
 import pandas as pd
+import wandb
+
+PUBLIC_WANDB_NAME = "opencompute"
+PUBLIC_WANDB_ENTITY = "neuralinternet"
+NETID = 27
+NETWORK = 'finney'
 
 def display_meta():
     # Initialize metagraph with your specific netid
-    netid = 27  # Replace with your actual netid
-    metagraph = bt.metagraph(netid, lite=True)
+
+    metagraph = bt.metagraph(NETID, lite=True, network = NETWORK)
     miner_version_summary = {}
     validator_version_summary = {}
 
@@ -20,14 +26,14 @@ def display_meta():
         v_trust = metagraph.validator_trust[index]
         v_permit = metagraph.validator_permit[index]
         active = metagraph.active[index]
-        if v_trust == 0:
+        if active == 0:
             data.append([index, hotkey, active, stake, trust, v_permit, v_trust, axon.ip, axon.port, axon.version])
             if axon.version in miner_version_summary:
                 miner_version_summary[axon.version] += 1
             else:
                 miner_version_summary[axon.version] = 1
         else:
-            val_version = metagraph.neurons[index].prometheus_info.version
+            val_version = get_validator_version(hotkey=hotkey)
             data.append([index, hotkey, active, stake, trust, v_permit, v_trust, axon.ip, axon.port, val_version])
             if val_version in validator_version_summary:
                 validator_version_summary[val_version] += 1
@@ -67,8 +73,33 @@ def display_meta():
     st.write(f'Total Miner Count: {miner_count}')
     st.dataframe(miner_summary_df)
 
+def get_validator_version(hotkey):
+    """
+    This function gets version from validator.
+    Only relevant for validators.
+    """
+    # Dictionary to store the (hotkey, specs) from wandb runs
+    db_specs_dict = {}
+
+    api = wandb.Api()
+    runs = api.runs(f"{PUBLIC_WANDB_ENTITY}/{PUBLIC_WANDB_NAME}",
+                        filters={"$and": [{"config.role": "validator"},
+                                          {"config.config.netuid": NETID},
+                                          {"config.hotkey": hotkey},
+                                          {"state": "running"},]
+                                })
+    try:
+        version = 0
+        # Iterate over all runs in the opencompute project
+        for index, run in enumerate(runs, start=1):
+            # Access the run's configuration
+            run_config = run.config
+            version = run_config.get('version', 0)
+        print(f"Hotkey: {hotkey}, Version: {version}")
+        return version
+    except Exception as e:
+        # Handle the exception by logging an error message
+        print(f"An error occurred while getting specs from wandb: {e}")
 
 if __name__ == '__main__':
     display_meta()
-
-
